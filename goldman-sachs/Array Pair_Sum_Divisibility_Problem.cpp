3e4f4c95c50267305d8c0482729fb682c39f31e2
Km Kalpana <85006350+KmKalpana@users.noreply.github.com>
2022-02-11 22:33:04 +0530

    Update Array Pair_Sum_Divisibility_Problem.cpp

--- goldman-sachs/Array Pair_Sum_Divisibility_Problem.cpp
+++ goldman-sachs/Array Pair_Sum_Divisibility_Problem.cpp
@@ -1,3 +1,10 @@
+/*Given an array of integers and a number k, write a function that returns true if given array can be divided into pairs such that sum of every pair is divisible by k.
+Input : arr = [9, 5, 7, 3], k = 6
+Output: True
+Explanation: {(9, 3), (5, 7)} is a 
+possible solution. 9 + 3 = 12 is divisible
+by 6 and 7 + 5 = 12 is also divisible by 6. */
+
 #include <bits/stdc++.h>
 using namespace std;
 
